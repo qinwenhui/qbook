c2306a96cfabc9a6377492666e8889de5bf19b08
qinwh <1510365643@qq.com>
2020-11-18 17:10:17 +0800

    init

---
+++ src/main/java/cn/qinwh/qbook/QbookApplication.java
@@ -0,0 +1,13 @@
+package cn.qinwh.qbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QbookApplication.class, args);
+    }
+
+}
