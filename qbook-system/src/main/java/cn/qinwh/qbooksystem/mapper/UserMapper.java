4921df92049d0f455ec14cc4b2405b73c1f7078f
qinwh <1510365643@qq.com>
2020-11-19 21:13:46 +0800

    mybatis逆向生成重新配置

---
+++ qbook-system/src/main/java/cn/qinwh/qbooksystem/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package cn.qinwh.qbooksystem.mapper;
+
+import cn.qinwh.qbooksystem.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
