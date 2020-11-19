3024c0a9f62b91729db9fc0d36a99d764b1d41ab
qinwh <1510365643@qq.com>
2020-11-19 17:30:22 +0800

    更新引入数据库

--- qbook-admin/src/main/java/cn/qinwh/qbookadmin/QbookAdminApplication.java
+++ qbook-admin/src/main/java/cn/qinwh/qbookadmin/QbookAdminApplication.java
@@ -3,7 +3,7 @@ package cn.qinwh.qbookadmin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"cn.qinwh.*"})
 public class QbookAdminApplication {
 
     public static void main(String[] args) {
